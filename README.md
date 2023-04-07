7ce3d760dce41870291fdb836e354c094b8818c8
spias8 <abullbashser20@gmail.com>
2023-04-07 23:22:04 +0600

    Android app upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Book-Shelf
+# My_Library
+It's a advance android project. You can read, download any kinds of books.
---
+++ app/src/main/java/com/pias/bookshelf/Constants.java
@@ -0,0 +1,6 @@
+package com.pias.bookshelf;
+
+public class Constants {
+
+    public static final long MAX_BYTES_PDF = 60000000;//60 MB
+}
